9453187a3faf62e9a5de23551954f0ed261c35ef
DarkblooM <darkbloompro@gmail.com>
2023-09-02 14:06:26 +0200

    add: Lua

---
+++ lua/passwdgen.lua
@@ -0,0 +1,16 @@
+math.randomseed(os.time())
+
+function get_length()
+  io.write("Password length (leave blank for default): ")
+  local uinput = io.read()
+  return tonumber(uinput) or 16
+end
+
+local password = ""
+local length = get_length()
+
+for n = 1, length do
+  password = password .. string.char(math.random(33, 126))
+end
+
+print(password)
